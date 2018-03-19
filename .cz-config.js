e7187947dd500ea37711c2cb6a35efd4bf4cbde3
Patrick Kollitsch <patrick@davids-neighbour.com>
2018-03-19 18:16:42 +0700

    build: theme v2018.1.133

--- .cz-config.js
+++ .cz-config.js
@@ -17,7 +17,11 @@ module.exports = {
       name: 'fix: A bug fix'
     },
     {
-      value: 'theme',
+      value: 'layout',
+      name: 'layout: changes to layout and theme files'
+    },
+    {
+      value: 'codestyle',
       name: 'style: Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)'
     },
     {
@@ -25,28 +29,12 @@ module.exports = {
       name: 'layout: changes to layout and theme files'
     },
     {
-      value: 'refactor',
-      name: 'refactor: A code change that neither fixes a bug nor adds a feature'
-    },
-    {
       value: 'perf',
       name: 'perf: A code change that improves performance'
     },
     {
-      value: 'test',
-      name: 'test: Adding missing tests'
-    },
-    {
       value: 'chore',
       name: 'chore: Changes to the build process or auxiliary tools\n            and libraries such as documentation generation'
-    },
-    {
-      value: 'revert',
-      name: 'revert: Revert to a commit'
-    },
-    {
-      value: 'WIP',
-      name: 'WIP: Work in progress'
     }
   ],
 
