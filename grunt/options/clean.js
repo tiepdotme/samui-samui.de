deb39504327574569f66f6e5c720c6803eae3d8f
Patrick Kollitsch <patrick@davids-neighbour.com>
2018-03-20 20:16:59 +0700

    chore: remove unused clean rules

--- grunt/options/clean.js
+++ grunt/options/clean.js
@@ -22,21 +22,6 @@ module.exports = {
         '<%= setup.theme %>/script*.js'
     ],
 
-    // clean theme directory and cache
-    theme: [
-        '<%= setup.app %>/wp-content/themes/wp-tropicalisland',
-        '<%= setup.app %>/wp-content/imagecache',
-        '<%= setup.app %>/wp-content/cache',
-        '<%= setup.app %>/wp-content/updraft/*.txt',
-        '<%= setup.app %>/wp-content/updraft/*.gz',
-        '<%= setup.app %>/wp-content/updraft'
-    ],
-
-    // clean debug log (prepare for staging)
-    debug: [
-        '<%= setup.app>/wp-content/debug.log'
-    ],
-
     // clean built files that are useless
     unused: [
         'dist/page',
