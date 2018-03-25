b6e93cd0b2e43cb3f4ec4fc8c66f061ce3f52e08
Patrick Kollitsch <patrick@davids-neighbour.com>
2018-03-25 17:30:12 +0700

    fix: grunt replace task stopped working

--- grunt/options/replace.js
+++ grunt/options/replace.js
@@ -1,6 +1,12 @@
 // replace version with current version number
 module.exports = {
     options: {
+        patterns: [
+            {
+                match: /[0-9]{1,4}\.[0-9]{1,2}\.[0-9]{1,4}/,
+                replacement: '<%= pkg.version %>'
+            }
+        ]
 
     },
     version: {
@@ -13,52 +19,5 @@ module.exports = {
                 overwrite: true
             }
         ],
-        patterns: [
-            {
-                match: /[0-9]{1,4}\.[0-9]{1,2}\.[0-9]{1,4}/,
-                replacement: '<%= pkg.version %>'
-            }
-        ]
-    },
-    encoding: {
-        files: [
-            {
-                expand: true,
-                cwd: 'content',
-                src: '**/*.md',
-                dest: './content/',
-                overwrite: true
-            }
-        ],
-        patterns: [
-            {
-                match: /\&Auml\;/,
-                replacement: 'Ä'
-            },
-            {
-                match: /\&auml\;/,
-                replacement: 'ä'
-            },
-            {
-                match: /\&Ouml\;/,
-                replacement: 'Ö'
-            },
-            {
-                match: /\&ouml\;/,
-                replacement: 'ö'
-            },
-            {
-                match: /\&Uuml\;/,
-                replacement: 'Ü'
-            },
-            {
-                match: /&uuml;/,
-                replacement: 'ü'
-            },
-            {
-                match: /\&szlig\;/,
-                replacement: 'ß'
-            }
-        ]
     }
 };
