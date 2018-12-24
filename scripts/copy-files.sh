22264c78446e1d2cd0fd88533935f91709fbe00c
Patrick Kollitsch <patrick@davids-neighbour.com>
2018-12-24 20:38:12 +0700

    feat(assets): create js and css files via hugo pipelines

---
+++ scripts/copy-files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp node_modules/jquery/dist/jquery.js assets/js/libs
+cp node_modules/popper.js/dist/umd/popper.js assets/js/libs
+cp node_modules/bootstrap/dist/js/bootstrap.js assets/js/libs
+cp node_modules/moment/moment.js assets/js/libs
+cp node_modules/instantsearch.js/dist/instantsearch.js assets/js/libs
+cp static/assets/fonts/fontawesome/js/fontawesome-all.min.js assets/js/libs
