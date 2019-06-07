65adbdd0432c649f296974ae5ff9f337b83cf6a2
Patrick Kollitsch <patrick@davids-neighbour.com>
2019-06-07 21:13:53 +0700

    feat: move analytics to internal template

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,6 @@
-
-function loadTagManager(){
-  var html = "<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-P8XWV7K');</script>";
-  jQuery('head').append(html);
-
-  var html2 = '<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P8XWV7K" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>';
-  jQuery('body').append(html2);
-
-}
-
 jQuery(document).ready(function ($) {
 
     'use strict';
-
-    // starting up tagmanager
-    window.dataLayer = window.dataLayer || [];
-    setTimeout("loadTagManager()", 500);
 
     // a little workaround for single blog posts and archive pages 
     // that are not automatically shown in active navigation
