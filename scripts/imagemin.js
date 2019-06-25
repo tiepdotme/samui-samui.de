bddc04c04ff241cb70cba69b71a6fb9d831625f8
Patrick Kollitsch <patrick@davids-neighbour.com>
2019-06-25 20:23:49 +0700

    chore: add a different jpg plugin to imagemin script

--- scripts/imagemin.js
+++ scripts/imagemin.js
@@ -1,13 +1,17 @@
 const imagemin = require('imagemin');
-const imageminJpegtran = require('imagemin-jpegtran');
 const imageminPngquant = require('imagemin-pngquant');
+const imageminMozjpeg = require('imagemin-mozjpeg');
 
 (async () => {
 	const files = await imagemin(['public/**/*.{jpg,jpeg,png}'], 'public/', {
 		plugins: [
-			imageminJpegtran(),
+			imageminMozjpeg({
+				quality: 75
+			}),
 			imageminPngquant({
-				quality: [0.6, 0.8]
+				quality: [0.6, 0.8],
+				speed: 1,
+				strip: true
 			})
 		]
 	});
