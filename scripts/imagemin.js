38eca9aa8aa3d757d3d0d346ce12a7e6d0cf2a06
Patrick Kollitsch <patrick@davids-neighbour.com>
2019-06-25 20:37:49 +0700

    chore: readd jpgtran for imagemin script

--- scripts/imagemin.js
+++ scripts/imagemin.js
@@ -1,10 +1,12 @@
 const imagemin = require('imagemin');
+const imageminJpegtran = require('imagemin-jpegtran');
 const imageminPngquant = require('imagemin-pngquant');
 const imageminMozjpeg = require('imagemin-mozjpeg');
 
 (async () => {
 	const files = await imagemin(['public/**/*.{jpg,jpeg,png}'], 'public/', {
 		plugins: [
+			imageminJpegtran(),
 			imageminMozjpeg({
 				quality: 75
 			}),
@@ -15,7 +17,5 @@ const imageminMozjpeg = require('imagemin-mozjpeg');
 			})
 		]
 	});
-
-	//console.log(files);
 	//=> [{data: <Buffer 89 50 4e …>, path: 'build/images/foo.jpg'}, …]
 })();
